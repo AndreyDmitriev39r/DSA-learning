37121d5906ce5d68d635f833b6db0f5270b15765
Andrey Dmitriev <admit8490@gmail.com>
2023-10-20 21:49:06 +0200

    sorting problems directory set

---
+++ sorting/swap.js
@@ -0,0 +1,7 @@
+// used in many sorting probs, like bubble or selection
+
+const swap = (array, firstIndex, secondIndex) => {
+  const temp = [array[secondIndex]];
+  array[secondIndex] = array[firstIndex];
+  array[firstIndex] = temp;
+}
