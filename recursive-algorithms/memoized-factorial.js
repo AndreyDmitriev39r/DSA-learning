279cf3005280bb75d429fbee736d86ebdabbdfba
Andrey Dmitriev <admit8490@gmail.com>
2023-11-14 17:00:45 +0200

    example from DO for memoized factorial added

--- recursive-algorithms/memoized-factorial.js
+++ recursive-algorithms/memoized-factorial.js
@@ -9,19 +9,35 @@
 // digital ocean example ??? too many calls, but try time measuring(they claim it is faster)
 // https://www.digitalocean.com/community/tutorials/js-understanding-recursion
 
-var memo = {};
-var factorial = function(n) {
-    if (n === 0) {
-        return 1;
-    } else if (memo.hasOwnProperty(n)) {
-        return memo[n];
-    } else {
-        console.log(`recursive call will be done, n is ${n}`)
-        var result = factorial(n - 1) * n;
-        memo[n] = result;
-        return result;
-    }
+const factorialFromDO = (n, memo) => {
+  memo = memo || {};
+  console.log(memo[n])
+  if (memo[n]) return memo[n];
+  if (n === 0) return 1;
+  for (let i = 0; i < n; i++) {    
+    memo[n] = n * factorialFromDO(n - 1, memo);
+  };
+
+  return memo[n];
 };
 
-factorial(2);
-factorial(5);
+console.log(factorialFromDO(2));  // 4 milliseconds
+// console.log(factorial(120));  // 12 milliseconds
+// console.log(factorial(1200)); // 24 milliseconds
+
+// var memo = {};
+// var factorial = function(n) {
+//     if (n === 0) {
+//         return 1;
+//     } else if (memo.hasOwnProperty(n)) {
+//         return memo[n];
+//     } else {
+//         console.log(`recursive call will be done, n is ${n}`)
+//         var result = factorial(n - 1) * n;
+//         memo[n] = result;
+//         return result;
+//     }
+// };
+
+// factorial(2);
+// factorial(5);
